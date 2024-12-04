dc44c2d40a781da3d353999b0a336742f30aecf9
Luximus <thomaslipman10@gmail.com>
2024-12-04 08:13:51 +0100

    day: ⭐ Day 4, 2024

---
+++ 2024/4_ceres-search/part-1.ts
@@ -0,0 +1,97 @@
+// Read the input file and split it into lines
+const input = await Deno.readTextFile("./input.txt");
+const lines: string[][] = input
+  .split("\n")
+  .map((line) => line.trim().split(""));
+
+// Get the max row and column
+const maxRow = lines.length;
+const maxCol = lines[0].length;
+
+// The target to search for
+const target = "XMAS";
+
+const isOutOfBounds = (row: number, col: number): boolean => {
+  return row < 0 || col < 0 || row >= maxRow || col >= maxCol;
+};
+
+const isLetterMatch = (row: number, col: number, letter: string): boolean => {
+  return !isOutOfBounds(row, col) && lines[row][col] === letter;
+};
+
+const StartLetterIsPartOfTarget = (row: number, col: number): number => {
+  // Object to store if the target is part of the direction
+  const isTarget = {
+    right: true,
+    bottom: true,
+    left: true,
+    top: true,
+    topRight: true,
+    bottomRight: true,
+    bottomLeft: true,
+    topLeft: true,
+  };
+
+  // Split the target into letters
+  const letters = target.split("");
+
+  // Loop through the letters of the target
+  letters.forEach((letter, index) => {
+    // Right
+    if (!isLetterMatch(row, col + index, letter)) {
+      isTarget.right = false;
+    }
+    // Bottom
+    if (!isLetterMatch(row + index, col, letter)) {
+      isTarget.bottom = false;
+    }
+    // Left
+    if (!isLetterMatch(row, col - index, letter)) {
+      isTarget.left = false;
+    }
+    // Top
+    if (!isLetterMatch(row - index, col, letter)) {
+      isTarget.top = false;
+    }
+    // Top-right
+    if (!isLetterMatch(row - index, col + index, letter)) {
+      isTarget.topRight = false;
+    }
+    // Bottom-right
+    if (!isLetterMatch(row + index, col + index, letter)) {
+      isTarget.bottomRight = false;
+    }
+    // Bottom-left
+    if (!isLetterMatch(row + index, col - index, letter)) {
+      isTarget.bottomLeft = false;
+    }
+    // Top-left
+    if (!isLetterMatch(row - index, col - index, letter)) {
+      isTarget.topLeft = false;
+    }
+  });
+
+  // Get all directions that are part of the target (filter by true)
+  const partOfNumberOfTargets = Object.values(isTarget).filter(
+    (value) => value
+  );
+
+  // Return the amount of directions that are part of the target
+  return partOfNumberOfTargets.length;
+};
+
+// Count the amount of targets found
+const amountOfTargets = lines.reduce(
+  (acc, line, row) =>
+    acc +
+    line
+      .map((cell, col) => {
+        if (cell !== target[0]) return 0;
+        return StartLetterIsPartOfTarget(row, col);
+      })
+      .reduce((acc, x) => acc + x, 0),
+  0
+);
+
+// Print the amount of targets found
+console.log(`Targets: ${amountOfTargets}`);
---
+++ 2024/4_ceres-search/part-2.ts
@@ -0,0 +1,72 @@
+// Read the input file and split it into lines
+const input = await Deno.readTextFile("./input.txt");
+const lines: string[][] = input
+  .split("\n")
+  .map((line) => line.trim().split(""));
+
+// Function to check if the given index is part of the target
+const StartLetterIsPartOfTarget = (row: number, col: number): boolean => {
+  // Make a 3x3 grid around the index
+  const grid: string[][] = [
+    [
+      lines[row - 1]?.[col - 1],
+      lines[row - 1]?.[col],
+      lines[row - 1]?.[col + 1],
+    ],
+    [lines[row]?.[col - 1], lines[row]?.[col], lines[row]?.[col + 1]],
+    [
+      lines[row + 1]?.[col - 1],
+      lines[row + 1]?.[col],
+      lines[row + 1]?.[col + 1],
+    ],
+  ];
+
+  // Return if the grid is not complete
+  if (grid.flat().length !== 3 * 3) return false;
+
+  // Starting at the top
+  const startsFromTop =
+    grid[0][0] === "M" && // M.M
+    grid[0][2] === "M" && // .A.
+    grid[1][1] === "A" && // S.S
+    grid[2][0] === "S" &&
+    grid[2][2] === "S";
+  // Starting at the left
+  const startsFromLeft =
+    grid[0][0] === "M" && // M.S
+    grid[0][2] === "S" && // .A.
+    grid[1][1] === "A" && // M.S
+    grid[2][0] === "M" &&
+    grid[2][2] === "S";
+  // Starting at the right
+  const startsFromRight =
+    grid[0][0] === "S" && // S.M
+    grid[0][2] === "M" && // .A.
+    grid[1][1] === "A" && // S.M
+    grid[2][0] === "S" &&
+    grid[2][2] === "M";
+  // Starting at the bottom
+  const startsFromBottom =
+    grid[0][0] === "S" && // S.S
+    grid[0][2] === "S" && // .A.
+    grid[1][1] === "A" && // M.M
+    grid[2][0] === "M" &&
+    grid[2][2] === "M";
+
+  // Return if any of the starts are true
+  return startsFromTop || startsFromLeft || startsFromRight || startsFromBottom;
+};
+
+// Count the amount of targets found
+const amountOfTargets = lines.reduce(
+  (acc, line, row) =>
+    acc +
+    line.reduce(
+      (acc, _, col) => acc + (StartLetterIsPartOfTarget(row, col) ? 1 : 0),
+      0
+    ),
+  0
+);
+
+// Print the amount of targets found
+console.log(`Targets: ${amountOfTargets}`);
